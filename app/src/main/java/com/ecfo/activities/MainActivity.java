1a348578b80366cc6d944d7431b4b51d8a305ffe
xueweipeng <xueweipeng-bd@ofo.com>
2018-05-08 14:01:06 +0800

    添加整理资源

--- app/src/main/java/com/ecfo/activities/MainActivity.java
+++ app/src/main/java/com/ecfo/activities/MainActivity.java
@@ -9,8 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.ecfo.R;
+import com.ecfo.modules.discovery.DiscoveryFragment;
 import com.ecfo.modules.lesson.LessonFragment;
 import com.ecfo.modules.me.MeFragment;
+import com.ecfo.modules.recommend.RecommendFragment;
+import com.ecfo.modules.warehouse.WarehouseFragment;
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
@@ -41,11 +44,20 @@ public class MainActivity extends AppCompatActivity {
     private void initFragment() {
         LessonFragment lessonFragment = LessonFragment.newInstance("", "");
         MeFragment meFragment = MeFragment.newInstance("", "");
+        RecommendFragment recommendFragment = RecommendFragment.newInstance("", "");
+        DiscoveryFragment discoveryFragment = DiscoveryFragment.newInstance("", "");
+        WarehouseFragment warehouseFragment = WarehouseFragment.newInstance("", "");
         List<TabViewChild> tabViewChildList = new ArrayList<>();
-        TabViewChild tabViewChild01 = new TabViewChild(R.drawable.lesson_sel, R.drawable.lesson_unsel, "课程", lessonFragment);
-        TabViewChild tabViewChild02 = new TabViewChild(R.drawable.me_sel, R.drawable.me_unsel, "我", meFragment);
+        TabViewChild tabViewChild01 = new TabViewChild(R.drawable.lesson_sel, R.drawable.lesson_unsel, "听课", lessonFragment);
+        TabViewChild tabViewChild02 = new TabViewChild(R.drawable.recommend_sel, R.drawable.recommend_unsel, "推荐", recommendFragment);
+        TabViewChild tabViewChild03 = new TabViewChild(R.drawable.discovery_sel, R.drawable.discovery_unsel, "发现", discoveryFragment);
+        TabViewChild tabViewChild04 = new TabViewChild(R.drawable.warehouse_sel, R.drawable.warehouse_unsel, "智库", warehouseFragment);
+        TabViewChild tabViewChild05 = new TabViewChild(R.drawable.me_sel, R.drawable.me_unsel, "我", meFragment);
         tabViewChildList.add(tabViewChild01);
         tabViewChildList.add(tabViewChild02);
+        tabViewChildList.add(tabViewChild03);
+        tabViewChildList.add(tabViewChild04);
+        tabViewChildList.add(tabViewChild05);
         tabView.setTabViewDefaultPosition(0);
         tabView.setTabViewChild(tabViewChildList, getSupportFragmentManager());
     }
