66398f005d8172438f6c54b8ce85f4c497eca605
xueweipeng <xueweipeng-bd@ofo.com>
2018-05-08 14:26:51 +0800

    增加数据库依赖

--- app/src/main/java/com/ecfo/ecfoApplication.java
+++ app/src/main/java/com/ecfo/ecfoApplication.java
@@ -4,11 +4,14 @@ import android.app.Application;
 
 import com.blankj.utilcode.util.Utils;
 
+import org.litepal.LitePal;
+
 
 public class ecfoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Utils.init(this);
+        LitePal.initialize(this);
     }
 }
